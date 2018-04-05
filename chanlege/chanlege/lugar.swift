16156e8165527598a7d404eafb7e15aa12e1a48f
kacioReis <khcb@academy.cin.ufpe.br>
2018-04-05 19:37:39 -0300

    Adicionando arquivos

---
+++ chanlege/chanlege/lugar.swift
@@ -0,0 +1,20 @@
+//
+//  lugar.swift
+//  chanlege
+//
+//  Created by Aluno on 05/04/18.
+//  Copyright © 2018 Aluno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+public class Lugar {
+    let nome:String
+    let posintionX:Double
+    let posintionY:Double
+    init(nome:String,posintionX:Double,posintionY:Double) {
+        self.nome = nome
+        self.posintionX = posintionX
+        self.posintionY = posintionY
+    }
+}
